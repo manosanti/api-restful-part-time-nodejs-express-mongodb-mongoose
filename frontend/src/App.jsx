2002f3ccac497db9ccbdd9bf677597f526186af1
Lucas Santiago <luccassantiago.contato@gmail.com>
2023-04-10 21:27:14 -0300

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,36 @@
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import Bilbo from './Components/Bilbo'
+import Cards from './Components/Cards'
+import styled from 'styled-components'
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <Bilbo>
+      <CardsGrid>
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+      </CardsGrid>
+      </Bilbo>
     </>
     )
 }
 
 export default App
+
+const CardsGrid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+    grid-column-gap: 0px;
+    grid-row-gap: 100px;
+`
